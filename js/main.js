4b99ca4d157274efa41945c3c03e6a7e4d7b5958
xiaomo3214 <meteor13579@yahoo.com.tw>
2020-12-31 17:06:08 +0800

    設定

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 var comic = $(".comics");
-var content = $("#main");
+var content = $(".content");
 var choice = $(".choice");
 var isPhone = detectmob();
+var choose = null, mouse = false;
+var startX = startY = endX = endY = 0;
+var story = document.querySelector(".comics");
+var over = document.querySelector(".over");
+var correct = document.querySelector(".correct");
+var wrong = document.querySelector(".wrong");
 
 getWidth();
-Mouse();
+mouseRead();
 
 //設定寬度
 function getWidth(event){
@@ -38,11 +44,8 @@ function detectmob() {
     }
 }
 
-
-var choose = null;
-var startX = startY = endX = endY = 0;
 //電腦指令
-function Mouse(){
+function mouseRead(){
     window.addEventListener('mousedown',function(event){
         event.preventDefault(); //防止預設觸控事件
     }, {passive: false});
@@ -72,100 +75,61 @@ function Mouse(){
     });
 
     window.addEventListener('mouseup',function(event){
-        switch(choose){
-            //主軸故事
-            case 'story':
-                choose = null;
-                break;
+        choose = null;
+        mouse = false;
+        startX = startY = endX = endY = 0;
+        // switch(choose){
+        //     //主軸故事
+        //     case 'story':
+        //         choose = null;
+        //         break;
 
-            //選錯選項
-            case 'gameover':
-                choose = null;
-                break;
+        //     //選錯選項
+        //     case 'gameover':
+        //         choose = null;
+        //         break;
             
-            //正確答案
-            case 'correctanswer':
-                choose = null;
-                break;
+        //     //正確答案
+        //     case 'correctanswer':
+        //         choose = null;
+        //         break;
 
-            //錯誤答案
-            case 'wronganswer':
-                choose = null;
-                break;
-        }
+        //     //錯誤答案
+        //     case 'wronganswer':
+        //         choose = null;
+        //         break;
+        // }
     },false);
+
+    story.addEventListener('mousedown',function(event){
+        // event.preventDefault();
+        mouse = true;
+        startX = event.screenX;
+        startY = event.screenY;
+        choose = 'story';
+    }, false);
+
+    over.addEventListener('mousedown',function(event){
+        // event.preventDefault();
+        mouse = true;
+        startX = event.screenX;
+        startY = event.screenY;
+        choose = 'gameover';
+    }, false);
+
+    correct.addEventListener('mousedown',function(event){
+        // event.preventDefault();
+        mouse = true;
+        startX = event.screenX;
+        startY = event.screenY;
+        choose = 'correctanswer';
+    }, false);
+
+    wrong.addEventListener('mousedown',function(event){
+        // event.preventDefault();
+        mouse = true;
+        startX = event.screenX;
+        startY = event.screenY;
+        choose = 'wronganswer';
+    }, false);
 }
-
-/*var sleep = $(".right");
-var choosesleep = document.querySelector(".right");
-var wakeup = $(".left");
-var choosewakeup = document.querySelector(".left");
-var startX = startY = endX = endY = 0;
-var choose = 0, mouse = false;
-//computer
-function mouseevent(){
-    choosesleep.addEventListener('mousedown',function(event){
-        // event.preventDefault();
-        mouse = true;
-        startX = event.screenX;
-        startY = event.screenY;
-        choose = 1;
-    }, false);
-    choosewakeup.addEventListener('mousedown',function(event){
-        // event.preventDefault();
-        mouse = true;
-        startX = event.screenX;
-        startY = event.screenY;
-        choose = 2;
-    }, false);
-    window.addEventListener('mousemove',function(event){
-        // event.preventDefault();
-        if(choose == 1){
-            var pos1 = sleep.offset();
-            endX = event.screenX;
-            endY = event.screenY;
-            var distanceX = (endX - startX);
-            // var distanceY = (endY - startY);
-            if(mouse && (startX!=Math.abs(distanceX)) && event.buttons == 1){
-                if(distanceX < 0){
-                    sleep.offset({left:pos1.left+distanceX});
-                }
-                if(distanceX > 0){
-                    sleep.offset({left:pos1.left+distanceX});
-                }
-                // if(distanceY < 0){
-                //     sleep.offset({top:pos1.top+distanceY});
-                // }
-                // if(distanceY > 0){
-                //     sleep.offset({top:pos1.top+distanceY});
-                // }
-                startX = endX;
-                // startY = endY;
-            }
-        }
-
-        if(choose == 2){
-            var pos1 = wakeup.offset();
-            endX = event.screenX;
-            endY = event.screenY;
-            var distanceX = (endX - startX);
-            // var distanceY = (endY - startY);
-            if(mouse && (startX!=Math.abs(distanceX)) && event.buttons == 1){
-                if(distanceX < 0){
-                    wakeup.offset({left:pos1.left+distanceX});
-                }
-                if(distanceX > 0){
-                    wakeup.offset({left:pos1.left+distanceX});
-                }
-                // if(distanceY < 0){
-                //     sleep.offset({top:pos1.top+distanceY});
-                // }
-                // if(distanceY > 0){
-                //     sleep.offset({top:pos1.top+distanceY});
-                // }
-                startX = endX;
-                // startY = endY;
-            }
-        }
-    }, false);
-}*/
